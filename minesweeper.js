f711de7618f8881fc97bb01ff6030ce0b141bf38
Mathieu <Mathieu.hj.anderson@gmail.com>
2016-10-31 14:11:58 +1300

    Add startGame for loop, add lib.getSurroundingCells

--- minesweeper.js
+++ minesweeper.js
@@ -3,15 +3,23 @@ document.addEventListener('DOMContentLoaded', startGame)
 // Define your `board` object here!
 var board = {
   cells: [
-    {col: 1, row: 1, isMine: true, hidden: true}, {col: 2, row: 1, isMine: false, hidden: true}, {col: 3, row: 1, isMine: false, hidden: true},
-    {col: 1, row: 2, isMine: false, hidden: true}, {col: 2, row: 2, isMine: true, hidden: true}, {col: 3, row: 2, isMine: false, hidden: true},
-    {col: 1, row: 3, isMine: false, hidden: true}, {col: 2, row: 3, isMine: false, hidden: true}, {col: 3, row: 3, isMine: true, hidden: true}
-  ],
-
-
+    {col: 1, row: 1, isMine: true, hidden: true},
+    {col: 2, row: 1, isMine: false, hidden: true},
+    {col: 3, row: 1, isMine: false, hidden: true},
+    {col: 1, row: 2, isMine: false, hidden: true},
+    {col: 2, row: 2, isMine: true, hidden: true},
+    {col: 3, row: 2, isMine: false, hidden: true},
+    {col: 1, row: 3, isMine: false, hidden: true},
+    {col: 2, row: 3, isMine: false, hidden: true},
+    {col: 3, row: 3, isMine: true, hidden: true}
+  ]
 };
 
 function startGame () {
+  var allCells = board.cells.length;
+  for (i =0; i < 1; i++){
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+    }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -31,9 +39,13 @@ function checkForWin () {
 // (there could be as many as 8). You don't have to get the surrounding
 // cells yourself! Just use `lib.getSurroundingCells`:
 //
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+//var surrounding = lib.getSurroundingCells(cell.row, cell.col)
 //
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+var surrounding = lib.getSurroundingCells(cell.row, cell.col);
+var count = 0;
+console.log(surrounding);
+
 }
