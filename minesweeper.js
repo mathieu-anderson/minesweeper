765edfb170e3d4b3dba217e6a0833aea28a0ba16
Mathieu <Mathieu.hj.anderson@gmail.com>
2016-10-31 14:34:41 +1300

    Change cells indexing, add prospective code for mine counting in comment

--- minesweeper.js
+++ minesweeper.js
@@ -3,21 +3,21 @@ document.addEventListener('DOMContentLoaded', startGame)
 // Define your `board` object here!
 var board = {
   cells: [
+    {col: 0, row: 0, isMine: true, hidden: true},
+    {col: 1, row: 0, isMine: false, hidden: true},
+    {col: 2, row: 0, isMine: false, hidden: true},
+    {col: 0, row: 1, isMine: false, hidden: true},
     {col: 1, row: 1, isMine: true, hidden: true},
     {col: 2, row: 1, isMine: false, hidden: true},
-    {col: 3, row: 1, isMine: false, hidden: true},
+    {col: 0, row: 2, isMine: false, hidden: true},
     {col: 1, row: 2, isMine: false, hidden: true},
-    {col: 2, row: 2, isMine: true, hidden: true},
-    {col: 3, row: 2, isMine: false, hidden: true},
-    {col: 1, row: 3, isMine: false, hidden: true},
-    {col: 2, row: 3, isMine: false, hidden: true},
-    {col: 3, row: 3, isMine: true, hidden: true}
+    {col: 2, row: 2, isMine: true, hidden: true}
   ]
 };
 
 function startGame () {
   var allCells = board.cells.length;
-  for (i =0; i < 1; i++){
+  for (i =0; i < allCells; i++){
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
     }
   // Don't remove this function call: it makes the game work!
@@ -47,5 +47,9 @@ function countSurroundingMines (cell) {
 var surrounding = lib.getSurroundingCells(cell.row, cell.col);
 var count = 0;
 console.log(surrounding);
-
+/*for (y = 0; y < surrounding.length; y++){
+  if (surrounding[y].isMine) {
+    return count++;
+  };
+}*/
 }
